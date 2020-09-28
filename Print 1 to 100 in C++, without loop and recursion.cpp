522b1e6873478755f5ed999775e4a6cbf3c2a8c1
guptaji6489 <guptaji6489@gmail.com>
2020-09-28 22:10:02 +0530

    Print 1 to 100 in C++, without loop and recursion

---
+++ Print 1 to 100 in C++, without loop and recursion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//int varc = printf("harshit");
+int main()
+{
+    int i=1;
+
+     label_1: if(i<=100){
+     cout<<i<<" ";
+     i++;
+     }
+     goto label_1;
+
+     cout<<endl;
+
+
+
+    return 0;
+}
