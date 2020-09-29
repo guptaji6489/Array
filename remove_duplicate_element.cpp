de852793aee5ee34dbbb4d21e6c1ea423e0f621c
guptaji6489 <guptaji6489@gmail.com>
2020-09-29 14:46:40 +0530

    remove duplicate element

---
+++ remove_duplicate_element.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void removeduplicate(int a[],int n)
+{
+    int j=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]!=a[i+1])
+        {
+            a[j]=a[i];
+            j++;
+        }
+    }
+    a[j]=a[n-1];
+
+    for(int i=0;i<=j;i++)
+    {
+        cout<<a[i]<<"   ";
+    }
+}
+int main()
+{
+    int a[]={1,2,2,2,3,4,5,5,5};
+    int n=9;
+    removeduplicate(a,n);
+}
